ee1966bebd653c199205ffefce51fc9ef7b2fd1f
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2023-12-17 19:18:24 +0530

    Add files via upload

---
+++ day07/RomanToInteger.java
@@ -0,0 +1,51 @@
+package day07;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int currValue = getValue(c);
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+
+    private static int getValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+}
